cb1cd12fc3684595e0d8238efc7fdd0a6b0fd124
Altaf$$456 <altafhussain4748@gmail.com>
2018-07-03 07:16:39 -0700

    Update password reset password done

--- app/src/main/java/com/example/altaf/rairiwala/Singelton/Constants.java
+++ app/src/main/java/com/example/altaf/rairiwala/Singelton/Constants.java
@@ -42,4 +42,6 @@ public class Constants {
     public  static  final String DELETEDELIVERYPERSON="https://rairriwala.000webhostapp.com/DeleteDeliveryPerson.php";
     public  static  final String ChangeAccount="https://rairriwala.000webhostapp.com/EditAccount.php";
     public  static  final String REJECTORDER="https://rairriwala.000webhostapp.com/RejectOrder.php";
+    public  static  final String RESETPASSWORD="https://rairriwala.000webhostapp.com/ResetPassword.php";
+
 }
