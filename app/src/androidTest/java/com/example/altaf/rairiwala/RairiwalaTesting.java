3a37356c4b5323399ce7d6a58c8c6d4ec80ab97c
Altaf$$456 <altafhussain4748@gmail.com>
2018-08-16 21:53:20 -0700

    Seller add product issue solved

--- app/src/androidTest/java/com/example/altaf/rairiwala/RairiwalaTesting.java
+++ app/src/androidTest/java/com/example/altaf/rairiwala/RairiwalaTesting.java
@@ -3,6 +3,7 @@ package com.example.altaf.rairiwala;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.example.altaf.rairiwala.AccountManagment.AppStartUpPage;
 import com.example.altaf.rairiwala.AccountManagment.UserLogin;
 
 import org.junit.Rule;
@@ -15,23 +16,26 @@ import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class RairiwalaTesting {
 
+
+    @Rule
+    public ActivityTestRule<AppStartUpPage> mActivityRule =
+            new ActivityTestRule<>(AppStartUpPage.class);
+
     @Test
     public void changeText_newActivity() {
-        onView(withId(R.id.login)).perform(click());
-       /* // Type text and then press the button.
-        onView(withId(R.id.inputField)).perform(typeText("NewText"),
+        onView(withId(R.id.loginHaveAccount)).perform(click());
+        onView(withId(R.id.login_phone_number)).perform(typeText("3470623241"),
                 closeSoftKeyboard());
-        onView(withId(R.id.switchActivity)).perform(click());
+        onView(withId(R.id.login_up_pin)).perform(typeText("1122"), closeSoftKeyboard());
+        onView(withId(R.id.login)).perform(click());
 
-        // This view is in a different Activity, no need to tell Espresso.
-        onView(withId(R.id.resultView)).check(matches(withText("NewText")));
-        onView(withId(R.id.helloWorld)).perform(click());*/
     }
 
 }
